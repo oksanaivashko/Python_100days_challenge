311e601a4a21b0c1ee0224257bb5274d89dcb218
oksanaivashko <oksivashko@gmail.com>
2022-11-02 23:01:13 -0500

    added calculator.py

---
+++ calculator.py
@@ -0,0 +1,8 @@
+# Calculate the total amount of two digits
+two_digit_number = input("Type a two digit number")
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+print(first_digit)
+print(second_digit)
+result = int(first_digit) + int(second_digit)
+print(result)
---
+++ list.py
@@ -0,0 +1,5 @@
+### Print data from lists
+fruits = ["Strawberries", "Spinach", "Kale", "Nectars", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vagetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes", "Carrot"]
+dirty_dozen = [fruits, vagetables]
+print(dirty_dozen)
