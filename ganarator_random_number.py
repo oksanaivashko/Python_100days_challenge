3dc77ff8a5e9fed83c2e26ebcd8dbb5129f4b0b2
oksanaivashko <oksivashko@gmail.com>
2022-11-05 21:56:06 -0500

    added genarator_random_number.py

---
+++ ganarator_random_number.py
@@ -0,0 +1,11 @@
+### Pseudorandom Number Genarator
+import random
+
+randomlntegeter = random.randint(1, 10)
+print(randomlntegeter)
+
+randomFloat = random.random() * 5
+print(randomFloat)
+
+love_score = random.randint(1, 10)
+print(f"Your love score is {love_score}")
